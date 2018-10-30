5bc46a4d90091ed6c99bf74c27b7153f6c003a3c
Earl Huskey <earlcoder@gmail.com>
2018-10-30 13:52:37 -0700

    Remove unused addresses

--- hookd/service.go
+++ hookd/service.go
@@ -14,10 +14,8 @@ type Service struct {
 // NewService returns new	ingest hook service
 func NewService(cfg *Config) (*Service, error) {
 	httpServerCfg := &HTTPServerConfig{
-		Addr:               cfg.Addr,
-		UserProfileRPCADDR: cfg.UserProfileRPCADDR,
-		CamerasRPCADDR:     cfg.CamerasRPCADDR,
-		ManagerRPCADDR:     cfg.ManagerRPCADDR,
+		Addr:           cfg.Addr,
+		ManagerRPCADDR: cfg.ManagerRPCADDR,
 	}
 	HTTPServer, err := NewHTTPServer(
 		httpServerCfg,
@@ -26,7 +24,6 @@ func NewService(cfg *Config) (*Service, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return &Service{
 		logger:     cfg.Logger,
 		cfg:        cfg,
